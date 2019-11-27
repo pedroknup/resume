5cb8af7d5684939159a100a68cabb97eec0a09b3
Pedro Knup <phknup@gmail.com>
2019-11-27 11:44:59 +0100

    Updates

---
+++ precache-manifest.e257656883a5f07acd24d260408295a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6517b79b589f3b69b4945e4ff6a1081e",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "a42527a34f4663c80fc3",
+    "url": "/resume/static/css/2.5bc09813.chunk.css"
+  },
+  {
+    "revision": "4ab8b964ad651e8da242",
+    "url": "/resume/static/css/main.b2e8e814.chunk.css"
+  },
+  {
+    "revision": "a42527a34f4663c80fc3",
+    "url": "/resume/static/js/2.cc74ad56.chunk.js"
+  },
+  {
+    "revision": "4ab8b964ad651e8da242",
+    "url": "/resume/static/js/main.afa15be5.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  }
+]);
