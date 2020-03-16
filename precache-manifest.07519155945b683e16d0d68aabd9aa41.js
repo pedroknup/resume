c0efc81ebadc6eda3d63f4d1019445ae739d31dc
pedroknup <phknup@gmail.com>
2020-03-16 15:26:36 +0000

    Deploying to gh-pages from  - 4e7cca89f3088490ca8ea5625775295e1b59d8e6 🚀

---
+++ precache-manifest.07519155945b683e16d0d68aabd9aa41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23799ee35570971c13572d3434117c17",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "a42527a34f4663c80fc3",
+    "url": "/resume/static/css/2.5bc09813.chunk.css"
+  },
+  {
+    "revision": "c234d4b3bd28244e0fa3",
+    "url": "/resume/static/css/main.0d64de07.chunk.css"
+  },
+  {
+    "revision": "a42527a34f4663c80fc3",
+    "url": "/resume/static/js/2.cc74ad56.chunk.js"
+  },
+  {
+    "revision": "c234d4b3bd28244e0fa3",
+    "url": "/resume/static/js/main.7043145e.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  }
+]);
